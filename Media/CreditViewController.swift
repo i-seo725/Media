774b12f2b8d1e39ca1ed50ca6196b0b0fc210d52
i-seo725 <hjing719@gmail.com>
2023-08-13 22:17:24 +0900

    레이아웃 설정

---
+++ Media/CreditViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.swift
+//  Media
+//
+//  Created by 이은서 on 2023/08/12.
+//
+
+import UIKit
+
+class CreditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+  
+}
